ef081403fab9c26cbb7fa52d422f4054f7275b3b
Muhammad Azmi <muhammad.azmi@ukirama.com>
2019-09-23 17:00:28 +0700

    Add remember login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
+
     before_save { self.email = email.downcase }
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -8,12 +10,32 @@ class User < ApplicationRecord
     
     has_secure_password
 
-    # Return hash digest
+    # Returns hash digest
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
         BCrypt::Password.create(string, cost: cost)
     end
 
+    # Returns random token
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    # Remember user token's
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    # Forget user
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
     
+    # Returns true if token matches
+    def authenticated?(remember_token)
+        return false if remember_digest.nil?
+        BCrypt::Password.new(remember_digest).is_password?(remember_token)
+    end
 end
